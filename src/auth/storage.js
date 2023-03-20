d94ded18dcd66437e336bec2e0ea932c1c23c255
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2023-03-19 20:33:49 -0400

    Added Token Encryption/Decryption

--- src/auth/storage.js
+++ src/auth/storage.js
@@ -1,19 +1,28 @@
+import crypto from 'crypto'
+
 const tokens = new Map()
-const whmcs = new Map()
+const algorithm = 'aes-256-cbc';
+const vector = crypto.randomBytes(16);
+const key = crypto.randomBytes(32)
+
+const cipher = crypto.createCipheriv(algorithm, key, vector)
+const decipher = crypto.createDecipheriv(algorithm, key, vector);
 
 export async function storeDiscordTokens(userId, token) {
-    await tokens.set(`discord-${userId}`, token)
+    const json = JSON.stringify(token)
+    await tokens.set(`discord-${userId}`, encrypt(json))
 }
 
 export async function getDiscordTokens(userId) {
-    return tokens.get(`discord-${userId}`)
+    const token = decrypt(await tokens.get(`discord-${userId}`))
+    console.log(token)
+    return JSON.parse(token)
 }
 
-export async function storeAntiForgeryToken(token, expire) {
-    await whmcs.set(`whmcs-${token}`, expire)
+function encrypt(plaintext) {
+    return cipher.update(plaintext, "utf-8", "hex") + cipher.final("hex");
 }
 
-export async function getAntiForgeryToken(token) {
-    return whmcs.get(`whmcs-${token}`)
+function decrypt(ciphertext) {
+    return decipher.update(ciphertext, "hex", "utf-8") + decipher.final("utf8");
 }
-
