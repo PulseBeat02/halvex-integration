e76d6d4d538594ed197244fe9f2727fc68c16cca
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2023-06-07 22:17:56 -0400

    Changes..

--- src/command/UnlinkCommand.js
+++ src/command/UnlinkCommand.js
@@ -1,4 +1,4 @@
-import {ActionRowBuilder, ButtonBuilder, Events} from 'discord.js'
+import {ActionRowBuilder, ButtonBuilder, EmbedBuilder, Events} from 'discord.js'
 import {getAccessToken, getDiscordToken} from '../auth/storage.js'
 import client from '../index.js'
 import config from '../config.js';
@@ -6,6 +6,8 @@ import fetch from 'node-fetch';
 import Command from "./Command.js";
 
 export default class UnlinkCommand extends Command {
+
+    originalInteraction
 
     constructor() {
         super("unlink", "Unlinks your Discord account from the Halvex panel");
@@ -17,22 +19,37 @@ export default class UnlinkCommand extends Command {
 
     async handleInteraction(interaction) {
         const user = interaction.user
-        if (!await this.checkExists(user)) {
-            await interaction.reply({content: 'You are not linked to a Halvex account.', ephemeral: true})
-            return
-        }
+        // if (!await this.checkExists(user)) {
+        //     await interaction.reply({content: 'You are not linked to a Halvex account.', ephemeral: true})
+        //     return
+        // }
         const yes = new ButtonBuilder()
             .setCustomId('yes')
             .setLabel('Yes')
-            .setStyle('SUCCESS')
+            .setStyle('Success')
         const no = new ButtonBuilder()
             .setCustomId('no')
             .setLabel('No')
-            .setStyle('DANGER')
+            .setStyle('Danger')
         const row = new ActionRowBuilder().addComponents(yes, no);
         client.on(Events.InteractionCreate, interaction => this.handleButtonInteraction(interaction));
-        const content = 'Are you sure you want to unlink your Discord account from the Halvex panel?'
-        await interaction.reply({content: content, components: [row]}, true)
+        const embed =
+            this.createEmbedMessage('Are you sure you want to unlink your Discord account from the Halvex panel?')
+        this.originalInteraction = interaction
+        await interaction.reply({embeds: [embed], components: [row], ephemeral: true})
+    }
+
+    async createEmbedMessage(description) {
+        return new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle('Halvex Unlinking')
+            .setAuthor({
+                name: 'Halvex Unlinking',
+                iconURL: 'https://halvex.net/img/header/logo.png',
+                url: 'https://halvex.net/index.html'
+            })
+            .setDescription(description)
+            .setTimestamp()
     }
 
     async handleButtonInteraction(interaction) {
@@ -42,28 +59,26 @@ export default class UnlinkCommand extends Command {
         if (interaction.customId === 'yes') {
             await this.handleUnlink(interaction)
         } else if (interaction.customId === 'no') {
-            await this.handleLink(interaction)
+            await this.handleLink()
         }
     }
 
-    async handleLink(interaction) {
-        interaction.reply({content: 'Cancelled unlinking process!', ephemeral: true})
+    async handleLink() {
+        const embed = this.createEmbedMessage('Cancelled unlinking process!')
+        await this.originalInteraction.editReply({
+            embeds: [embed], components: [], ephemeral: true
+        })
     }
 
     async handleUnlink(interaction) {
         const url = `https://discord.com/api/v10/users/@me/applications/${config.DISCORD_CLIENT_ID}/role-connection`;
-        const user = interaction.user
+        const user = interaction.member
         const userId = user.userId
         const accessToken = getDiscordToken(userId)
         const method = 'PUT';
         const body = JSON.stringify(this.getMetaDataBody());
         const headers = this.getMetaDataHeaders(accessToken);
-        const response = await this.fetchMetaDataResponse(
-            url,
-            method,
-            body,
-            headers
-        );
+        const response = await this.fetchMetaDataResponse(url, method, body, headers);
         let role;
         for (const roles of user.roles.cache) {
             if (roles.name === 'Active Client') {
@@ -74,13 +89,17 @@ export default class UnlinkCommand extends Command {
             await user.roles.delete(role)
         }
         if (response.ok) {
-            await interaction.reply({
-                content: 'Your Discord account has been unlinked from the Halvex panel!',
+            const embed = this.createEmbedMessage('Your Discord account has been unlinked from the Halvex panel!')
+            await this.originalInteraction.editReply({
+                embeds: [embed],
+                components: [],
                 ephemeral: true
             })
         } else {
-            await interaction.reply({
-                content: 'There was an error unlinking your Discord account from the Halvex panel. Please try again later.',
+            const embed = this.createEmbedMessage('There was an error unlinking your Discord account from the Halvex panel. Please try again later.')
+            await this.originalInteraction.editReply({
+                embeds: [embed],
+                components: [],
                 ephemeral: true
             })
         }
@@ -88,23 +107,19 @@ export default class UnlinkCommand extends Command {
 
     async getMetaDataHeaders(accessToken) {
         return {
-            Authorization: `Bearer ${accessToken}`,
-            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`, 'Content-Type': 'application/json',
         };
     }
 
     async fetchMetaDataResponse(url, method, body, headers) {
         return fetch(url, {
-            method: method,
-            body: body,
-            headers: headers,
+            method: method, body: body, headers: headers,
         });
     }
 
     async getMetaDataBody() {
         return {
-            platform_name: 'Halvex Linker Bot',
-            halvexservices: 0,
+            platform_name: 'Halvex Linker Bot', halvexservices: 0,
         };
     }
 
