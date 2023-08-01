aeaea487204d5948fed10b99a4202819eb769cfa
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2023-07-31 23:20:19 -0400

    Some Final Changes

--- src/auth/storage.js
+++ src/auth/storage.js
@@ -59,8 +59,3 @@ function decrypt(ciphertext) {
   const decipher = crypto.createDecipheriv(algorithm, key, vector);
   return decipher.update(ciphertext, 'hex', 'utf-8') + decipher.final('utf8');
 }
-
-// Write a function that converts all the maps into json files
-function deserialize() {
-
-}
